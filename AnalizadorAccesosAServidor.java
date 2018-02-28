9f1757727edae73888cff1ee163edba9dac65a57
jorgevff <jvalladaresfontano@gmail.com>
2018-02-28 08:23:39 +0100

    Metodo que devuelve la hora con mas accesos al servidor y la muestra por pantalla

--- AnalizadorAccesosAServidor.java
+++ AnalizadorAccesosAServidor.java
@@ -3,6 +3,7 @@ import java.util.Iterator;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 
 /**
  * Write a description of class AnalizadorAccesoAServidor here.
@@ -23,12 +24,14 @@ public class AnalizadorAccesosAServidor
         // initialise instance variables
         accesos = new ArrayList<>();
     }
+
     /**
      * Metodo que lee un archivo log pasado por parametro
      */
 
     public void analizarArchivoDeLog(String archivoLog)
     {
+        accesos.clear();
 
         try {
             File archivoALeer = new File(archivoLog);
@@ -41,7 +44,8 @@ public class AnalizadorAccesosAServidor
                         Integer.parseInt(accesosPorLinea[1]), 
                         Integer.parseInt(accesosPorLinea[2]),
                         Integer.parseInt(accesosPorLinea[3]), 
-                        Integer.parseInt(accesosPorLinea[4]));                                           
+                        Integer.parseInt(accesosPorLinea[4]));
+                //se añaden los objetos a nuestro ArrayList        
                 accesos.add(accesoActual);
             }
             sc.close();
@@ -51,7 +55,46 @@ public class AnalizadorAccesosAServidor
         }
 
     }
-    
-    
+
+    /**
+     * Metodo que devuelve la hora con mas accesos al servidor, y que devuelve -1 si no hay ningun acceso. 
+     */
+    public int obtenerHoraMasAccesos()
+    {
+        int horaConMasAccesos = -1;
+
+        HashSet<Integer> horas = new HashSet<>();
+
+        int vecesMasAltaRepetida = 0;
+        if(accesos.size() > 0){
+            for(Acceso accesoActual : accesos){
+
+                int nuevaHoraAcceso = accesoActual.obtenerHora();
+                horas.add(nuevaHoraAcceso);
+
+            }
+            for(Integer horaActual: horas){
+                int numeroHorasRepetidas = 0;
+                for(Acceso nuevoAcceso : accesos){
+                    if(nuevoAcceso.obtenerHora() == horaActual){
+                        numeroHorasRepetidas++;                 
+                    }
+
+                }
+                if(numeroHorasRepetidas > vecesMasAltaRepetida){
+                    vecesMasAltaRepetida = numeroHorasRepetidas;
+                    horaConMasAccesos = horaActual;
+
+                }
+                if(numeroHorasRepetidas == vecesMasAltaRepetida && horaConMasAccesos < horaActual){
+                    horaConMasAccesos = horaActual;
+
+                }
+            }
+
+        }
+        System.out.println("La hora con mas accesos al servidor son las: " + horaConMasAccesos);
+        return horaConMasAccesos; 
+    }
 
 }
